2444b9bbbad215afc0402d1fe8cee7c09c8c8922
yangling8023 <37821717+yangling8023@users.noreply.github.com>
2018-04-02 15:19:22 +0800

    upload project

---
+++ springBoot/src/main/java/com/gc/yangling/TestSpringBoot.java
@@ -0,0 +1,12 @@
+package com.gc.yangling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSpringBoot {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestSpringBoot.class, args);
+	}
+}
